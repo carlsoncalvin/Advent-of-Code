3a5bca2429984920ca26d438d49cb4a803621f12
carlsoncalvin <carlsoncalvin@gmail.com>
2025-12-03 14:31:04 +0100

    Start of refactor into functions and scripts. Day 1 completed

---
+++ 2025/src/aoc2025/day01.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import math
+
+DATA_DIR = Path(__file__).resolve().parents[2] / "data"
+
+DIAL_LENGTH = 100
+DIRECTION = {"R": 1, "L": -1}
+
+def parse_input(text: str) -> list[str]:
+    return text.strip().splitlines()
+
+def solve_part1(lines: list[str]) -> int:
+    position = 50
+    zero_count = 0
+
+    for line in lines:
+        direction_change = DIRECTION[line[0]]
+        steps = int(line[1:])
+        delta = direction_change * steps
+
+        position = (position + delta) % DIAL_LENGTH
+
+        if position == 0:
+            zero_count += 1
+
+    return zero_count
+
+def solve_part2(lines: list[str]) -> int:
+    '''
+    A nice implementation that uses intervals to avoid counting crossings with modulo arithmetic
+    and conditionals for each eventuality. Also uses an unbounded range rather than resetting to a
+    "real" position at the end of each move.
+    '''
+    position = 50
+    crossings = 0
+
+    for line in lines:
+        direction_change = DIRECTION[line[0]]
+        steps = int(line[1:])
+
+        # define an ordered interval [a, b] for the move describing positions visited
+        # EXCLUDING the starting point
+        if direction_change == 1:
+            a = position + 1
+            b = position + steps
+        else:
+            a = position - steps
+            b = position - 1
+
+        # count how many crossings in [a, b]
+        # ceiling for min and floor for max
+        # if not a multiple of 100 in the range then the conditional fails
+        k_min = math.ceil(a / DIAL_LENGTH)
+        k_max = math.floor(b / DIAL_LENGTH)
+        if k_max >= k_min:
+            crossings += k_max - k_min + 1
+
+        position += direction_change * steps
+
+    return crossings
+
+def main() -> None:
+    input_path = DATA_DIR / "day01.txt"
+    text = input_path.read_text(encoding="utf-8")
+    lines = parse_input(text)
+
+    print("Day 1")
+    print("Part 1:", solve_part1(lines))
+    print("Part 2:", solve_part2(lines))
+
+if __name__ == "__main__":
+    main()
