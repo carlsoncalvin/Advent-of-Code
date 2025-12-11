5b3b7f7be2fc3d7324da41e850f1055dc65e321d
carlsoncalvin <carlsoncalvin@gmail.com>
2025-12-11 13:28:12 +0100

    Solved day 11

--- 2025/src/aoc2025/day11.py
+++ 2025/src/aoc2025/day11.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import datetime
+import functools
 
 # constants
 DATA_DIR = Path(__file__).resolve().parents[2] / "data"
 
 # parser
-def parse_input(text: str) -> dict[str, str]:
+def parse_input(text: str) -> dict[str, list[str]]:
     lines = text.strip().splitlines()
     device_dict = {}
     for line in lines:
@@ -13,59 +14,86 @@ def parse_input(text: str) -> dict[str, str]:
         device_dict[device] = connections.split()
     return device_dict
 
-def dfs(start: str, end: str, device_dict: dict[str, str], visited=None,
-        all_paths=None, part2 = False, seen_dac=False) -> list[list[str]] | None:
+def dfs(start: str, end: str, device_dict: dict[str, list[str]],
+        visited=None, all_paths=None,) -> list[list[str]] | None:
+    """
+    My first deep first search algorithm. Runs fast on part 1. I tried modifying for part 2 but
+    found a much better solution.
+    """
     if visited is None:
         visited = [start]
     if all_paths is None:
         all_paths = []
+    # exit recursion
     if start == end:
-        if part2 and "fft" in visited:
-            print("found path")
-            all_paths.append(list(visited))
-        elif not part2:
-            all_paths.append(list(visited))
+        all_paths.append(list(visited))
         return None
 
     for node in device_dict[start]:
-        # dac comes first in my set
-        if part2 and node == "dac":
-            seen_dac = True
-        # if we encounter fft before dac we break
-        if part2 and node == "fft" and not seen_dac:
-            return None
         if node not in visited:
             visited.append(node)
-            dfs(node, end,  device_dict, visited, all_paths, part2=part2, seen_dac=seen_dac)
+            dfs(node, end,  device_dict, visited, all_paths)
+            # pop path after end is reached
             visited.pop()
 
     return all_paths
 
-def solve_part1(device_dict: dict[str, str]) -> int:
+def solve_part1(device_dict: dict[str, list[str]]) -> int:
+    """Find all paths and return the length of the list."""
     all_paths = dfs("you", "out", device_dict)
     return len(all_paths)
 
-def solve_part2(device_dict: dict[str, str]) -> int:
-    start_time = datetime.datetime.now()
-    print(start_time.time(), "part 2 started")
+# part 2:
+# I couldn't figure out manual memoization but I found this caching decorator that works
+@functools.lru_cache(maxsize=None)
+def dfs_part2(node: str, target_tracker: tuple[int, int]) -> int:
+    """
+    The big realization here was that I don't need to actually track the paths traversed. I
+    discovered that the graph is "directed acyclic" (thanks reddit) so I don't have to worry about
+    getting caught in a loop. I can just track the number of times we hit the dac and fft nodes from
+    any given path and cache the result. Then, at the end, sum the bool results.
+    """
+    # check if we passed targets once reaching end
+    if node == "out":
+        return target_tracker[0] + target_tracker[1] == 2
 
-    all_paths = dfs("svr", "out", device_dict, part2=True)
+    # check if we hit dac or fft and update target tracker
+    if node == "dac":
+        target_tracker = (1, target_tracker[1])
+    elif node == "fft":
+        target_tracker = (target_tracker[0], 1)
+
+    # do recursion
+    total = 0
+    for next_node in DEVICE_DICT[node]:
+        total += dfs_part2(next_node, target_tracker)
+    return total
+
+def solve_part2() -> int:
+    """
+    Runs in less than 0.4 ms. Wasted a lot of time waiting for code to run before figuring out
+    caching.
+    """
+    start_time = datetime.datetime.now()
+
+    num_paths = dfs_part2("svr", (0, 0))
 
     end_time = datetime.datetime.now()
-    print(end_time.time(), "part 2 ended")
-    total_time = end_time - start_time
-    print(total_time, "total time")
+    print(end_time - start_time, "total time")
 
-    return len(all_paths)
+    return num_paths
 
 def main() -> None:
     input_path = DATA_DIR / "day11.txt"
     text = input_path.read_text(encoding="utf-8")
-    device_dict = parse_input(text)
+
+    # have to make the input global so it doesn't need to be hashed in part 2
+    global DEVICE_DICT
+    DEVICE_DICT = parse_input(text)
 
     print("Day 11")
-    print("Part 1:", solve_part1(device_dict))
-    print("Part 2:", solve_part2(device_dict))
+    print("Part 1:", solve_part1(DEVICE_DICT))
+    print("Part 2:", solve_part2())
 
 if __name__ == "__main__":
     main()
